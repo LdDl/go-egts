ce21531afb14e23477567131b9308e31d6286e64
Dimitrii <sexykdi@gmail.com>
2019-09-03 12:35:32 +0300

    EGTS_SR_STATE_DATA encoding to bytes

--- egts/subrecord/sr_state_data.go
+++ egts/subrecord/sr_state_data.go
@@ -2,6 +2,7 @@ package subrecord
 
 import (
 	"fmt"
+	"strconv"
 )
 
 // SRStateData EGTS_SR_STATE_DATA
@@ -18,9 +19,12 @@ type SRStateData struct {
 	InternalBatteryEnable  string `json:"IBU"` /* Internal Battery Used */
 	BackupBatteryEnable    string `json:"BBU"` /* Back Up Battery Used */
 	/* Data section */
-	MainPowerSourceVoltage float32 `json:"MPSV"` /* Main Power Source Voltage, in 0.1V */
-	BackupBatteryVoltage   float32 `json:"BBV"`  /* Back Up Battery Voltage, in 0.1V */
-	InternalBatteryVoltage float32 `json:"IBV"`  /* Internal Battery Voltage, in 0.1V */
+	MainPowerSourceVoltage     float32 `json:"MPSV_value"` /* Main Power Source Voltage, in 0.1V */
+	BackupBatteryVoltage       float32 `json:"BBV_value"`  /* Back Up Battery Voltage, in 0.1V */
+	InternalBatteryVoltage     float32 `json:"IBV_value"`  /* Internal Battery Voltage, in 0.1V */
+	MainPowerSourceVoltageByte uint8   `json:"MPSV"`       /* Main Power Source Voltage, in 0.1V */
+	BackupBatteryVoltageByte   uint8   `json:"BBV"`        /* Back Up Battery Voltage, in 0.1V */
+	InternalBatteryVoltageByte uint8   `json:"IBV"`        /* Internal Battery Voltage, in 0.1V */
 }
 
 // Decode Parse array of bytes to EGTS_SR_STATE_DATA
@@ -28,9 +32,13 @@ func (subr *SRStateData) Decode(b []byte) {
 	states := []string{"Idle", "EraGlonass", "Active", "EmergencyCall", "EmergencyMonitor", "Testing", "Service", "FirmwareUpdate"}
 	subr.State = states[int(b[0])]
 	subr.StateByte = b[0]
+
 	subr.MainPowerSourceVoltage = float32(b[1]) * 0.1
 	subr.BackupBatteryVoltage = float32(b[2]) * 0.1
 	subr.InternalBatteryVoltage = float32(b[3]) * 0.1
+	subr.MainPowerSourceVoltageByte = b[1]
+	subr.BackupBatteryVoltageByte = b[2]
+	subr.InternalBatteryVoltageByte = b[3]
 
 	flagByte := uint16(b[4])
 	flagByteAsBits := fmt.Sprintf("%08b", flagByte)
@@ -41,6 +49,15 @@ func (subr *SRStateData) Decode(b []byte) {
 
 // Encode Parse EGTS_SR_STATE_DATA to array of bytes
 func (subr *SRStateData) Encode() (b []byte) {
+	b = append(b, subr.StateByte)
+	b = append(b, subr.MainPowerSourceVoltageByte)
+	b = append(b, subr.BackupBatteryVoltageByte)
+	b = append(b, subr.InternalBatteryVoltageByte)
+
+	flagsBits := subr.NavigationModuleEnable + subr.InternalBatteryEnable + subr.BackupBatteryEnable
+	flags := uint64(0)
+	flags, _ = strconv.ParseUint(flagsBits, 2, 8)
+	b = append(b, uint8(flags))
 	return b
 }
 
