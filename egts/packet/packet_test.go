c97a489b6ca76dcc1634f18abdd3342ae5b73322
Dimitrii <sexykdi@gmail.com>
2019-09-03 12:24:25 +0300

    upd test case for identity

--- egts/packet/packet_test.go
+++ egts/packet/packet_test.go
@@ -7,24 +7,47 @@ import (
 	"testing"
 )
 
+type Identity struct {
+	Incoming  string
+	Code      uint8
+	Outcoming string
+}
+
+var (
+	identities = []Identity{
+		Identity{
+			Incoming:  "0100020b0020000000014f1900000010010101160000000000523836363130343032393639303030380004417f",
+			Code:      0,
+			Outcoming: "0100030b001000000000b300000006000000580101000300000000d9d1",
+		},
+		Identity{
+			Incoming:  "0100020b0020000000014f19000000100101011600000000005238363637393530333034383630353200045155",
+			Code:      0,
+			Outcoming: "0100030b001000000000b300000006000000580101000300000000d9d1",
+		},
+	}
+)
+
 func TestIdentity(t *testing.T) {
-	egtsAuthHex := "0100020b0020000000014f1900000010010101160000000000523836363130343032393639303030380004417f"
-	egtsAuth, err := hex.DecodeString(egtsAuthHex)
-	if err != nil {
-		t.Errorf("Error: %s", err.Error())
-	}
+	for i := range identities {
+		egtsAuthHex := identities[i].Incoming
+		egtsAuth, err := hex.DecodeString(egtsAuthHex)
+		if err != nil {
+			t.Errorf("Error: %s", err.Error())
+		}
 
-	parsedAuth, authCode := ReadPacket(egtsAuth)
-	if authCode != 0 {
-		t.Errorf("Auth code should be 0, but got %d", authCode)
-	}
+		parsedAuth, authCode := ReadPacket(egtsAuth)
+		if authCode != identities[i].Code {
+			t.Errorf("Auth code should be %d, but got %d", identities[i].Code, authCode)
+		}
 
-	resp := parsedAuth.PrepareAnswer()
-	encodedResp := resp.Encode()
-	hexResp := hex.EncodeToString(encodedResp)
-	trueResp := "0100030b001000000000b300000006000000580101000300000000d9d1"
-	if hexResp != trueResp {
-		t.Errorf("Response should be %s, but got %s", trueResp, hexResp)
+		resp := parsedAuth.PrepareAnswer()
+		encodedResp := resp.Encode()
+		hexResp := hex.EncodeToString(encodedResp)
+		trueResp := identities[i].Outcoming
+		if hexResp != trueResp {
+			t.Errorf("Response should be %s, but got %s", trueResp, hexResp)
+		}
 	}
 }
 
