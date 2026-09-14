3cb149c7b82221f6eb544f0d430e39464cfccd49
Dimitrii Lopanov <sexykdi@gmail.com>
2026-09-14 22:51:20 +0300

    no more flag changes

--- egts/subrecord/sr_ad_sensors_data.go
+++ egts/subrecord/sr_ad_sensors_data.go
@@ -4,8 +4,8 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io"
 	"strconv"
-	"strings"
 )
 
 // SRAdSensorsData EGTS_SR_AD_SENSORS_DATA
@@ -31,64 +31,74 @@ type SRAdSensorsData struct {
 func (subr *SRAdSensorsData) Decode(b []byte) (err error) {
 
 	buffer := bytes.NewReader(b)
+	decoded := SRAdSensorsData{}
 
 	flagByteADI := byte(0)
-	if flagByteADI, err = buffer.ReadByte(); err != nil {
-		return fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error reading flags ADI")
+	flagByteADI, err = buffer.ReadByte()
+	if err != nil {
+		return fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error reading flags ADI: %w", err)
 	}
 	flagByteAsBitsADI := fmt.Sprintf("%08b", flagByteADI)
 
 	// DIOE1 ... DIOE8 - Digital Inputs Octet Exists
-	subr.DIOExists = make([]string, 8) // not [8]string{}, because slice is needed in Encode() method
-	subr.DIOExists[0] = flagByteAsBitsADI[7:]
-	subr.DIOExists[1] = flagByteAsBitsADI[6:7]
-	subr.DIOExists[2] = flagByteAsBitsADI[5:6]
-	subr.DIOExists[3] = flagByteAsBitsADI[4:5]
-	subr.DIOExists[4] = flagByteAsBitsADI[3:4]
-	subr.DIOExists[5] = flagByteAsBitsADI[2:3]
-	subr.DIOExists[6] = flagByteAsBitsADI[1:2]
-	subr.DIOExists[7] = flagByteAsBitsADI[:1]
+	decoded.DIOExists = make([]string, 8)
+	decoded.DIOExists[0] = flagByteAsBitsADI[7:]
+	decoded.DIOExists[1] = flagByteAsBitsADI[6:7]
+	decoded.DIOExists[2] = flagByteAsBitsADI[5:6]
+	decoded.DIOExists[3] = flagByteAsBitsADI[4:5]
+	decoded.DIOExists[4] = flagByteAsBitsADI[3:4]
+	decoded.DIOExists[5] = flagByteAsBitsADI[2:3]
+	decoded.DIOExists[6] = flagByteAsBitsADI[1:2]
+	decoded.DIOExists[7] = flagByteAsBitsADI[:1]
 
 	// Digital Outputs
-	if subr.DigitalOutputs, err = buffer.ReadByte(); err != nil {
-		return fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error reading DOUT")
+	decoded.DigitalOutputs, err = buffer.ReadByte()
+	if err != nil {
+		return fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error reading DOUT: %w", err)
 	}
 
 	flagByteANS := byte(0)
-	if flagByteANS, err = buffer.ReadByte(); err != nil {
-		return fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error reading flags ANS")
+	flagByteANS, err = buffer.ReadByte()
+	if err != nil {
+		return fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error reading flags ANS: %w", err)
 	}
 	flagByteAsBitsANS := fmt.Sprintf("%08b", flagByteANS)
 	// ASFE1 ... ASFE8 - (Analog Sensor Field Exists)
-	subr.ANSExists = make([]string, 8) // not [8]string{}, because slice is needed in Encode() method
-	subr.ANSExists[0] = flagByteAsBitsANS[7:]
-	subr.ANSExists[1] = flagByteAsBitsANS[6:7]
-	subr.ANSExists[2] = flagByteAsBitsANS[5:6]
-	subr.ANSExists[3] = flagByteAsBitsANS[4:5]
-	subr.ANSExists[4] = flagByteAsBitsANS[3:4]
-	subr.ANSExists[5] = flagByteAsBitsANS[2:3]
-	subr.ANSExists[6] = flagByteAsBitsANS[1:2]
-	subr.ANSExists[7] = flagByteAsBitsANS[:1]
+	decoded.ANSExists = make([]string, 8)
+	decoded.ANSExists[0] = flagByteAsBitsANS[7:]
+	decoded.ANSExists[1] = flagByteAsBitsANS[6:7]
+	decoded.ANSExists[2] = flagByteAsBitsANS[5:6]
+	decoded.ANSExists[3] = flagByteAsBitsANS[4:5]
+	decoded.ANSExists[4] = flagByteAsBitsANS[3:4]
+	decoded.ANSExists[5] = flagByteAsBitsANS[2:3]
+	decoded.ANSExists[6] = flagByteAsBitsANS[1:2]
+	decoded.ANSExists[7] = flagByteAsBitsANS[:1]
 
-	for i := range subr.DIOExists {
-		if subr.DIOExists[i] == "1" {
-			if subr.ADI[i], err = buffer.ReadByte(); err != nil {
-				return fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error reading flags ADI")
+	for i := range decoded.DIOExists {
+		if decoded.DIOExists[i] == "1" {
+			decoded.ADI[i], err = buffer.ReadByte()
+			if err != nil {
+				return fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error reading ADI%d: %w", i+1, err)
 			}
 		}
 	}
 
-	for i := range subr.ANSExists {
-		if subr.ANSExists[i] == "1" {
+	for i := range decoded.ANSExists {
+		if decoded.ANSExists[i] == "1" {
 			ans := make([]byte, 3)
-			if _, err = buffer.Read(ans); err != nil {
-				return fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error reading flags ANS")
+			_, err = io.ReadFull(buffer, ans)
+			if err != nil {
+				return fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error reading ANS%d: %w", i+1, err)
 			}
 			ans = append(ans, 0x00)
-			subr.ANS[i] = binary.LittleEndian.Uint32(ans)
+			decoded.ANS[i] = binary.LittleEndian.Uint32(ans)
 		}
 	}
 
+	if buffer.Len() != 0 {
+		return fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Unexpected trailing data")
+	}
+	*subr = decoded
 	return nil
 }
 
@@ -96,52 +106,71 @@ func (subr *SRAdSensorsData) Decode(b []byte) (err error) {
 func (subr *SRAdSensorsData) Encode() (b []byte, err error) {
 	buffer := new(bytes.Buffer)
 
+	if len(subr.DIOExists) != len(subr.ADI) {
+		return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Expected 8 ADI flags")
+	}
+	if len(subr.ANSExists) != len(subr.ANS) {
+		return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Expected 8 ANS flags")
+	}
+	flagsBitsDIO := ""
+	for i := len(subr.DIOExists) - 1; i >= 0; i-- {
+		if subr.DIOExists[i] != "0" && subr.DIOExists[i] != "1" {
+			return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Invalid ADI flag %d", i+1)
+		}
+		flagsBitsDIO += subr.DIOExists[i]
+	}
+
 	flagsDIO := uint64(0)
-
-	for i := len(subr.DIOExists)/2 - 1; i >= 0; i-- { // reversed order of DIO
-		opp := len(subr.DIOExists) - 1 - i
-		subr.DIOExists[i], subr.DIOExists[opp] = subr.DIOExists[opp], subr.DIOExists[i]
-	}
-
-	flagsDIO, err = strconv.ParseUint(strings.Join(subr.DIOExists, ""), 2, 8)
+	flagsDIO, err = strconv.ParseUint(flagsBitsDIO, 2, 8)
 	if err != nil {
-		return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error writing flags ADI")
+		return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error writing flags ADI: %w", err)
 	}
-	if err = buffer.WriteByte(uint8(flagsDIO)); err != nil {
-		return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error writing byte flags ADI")
-	}
-
-	if err = buffer.WriteByte(subr.DigitalOutputs); err != nil {
-		return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error writing DOUT")
+	err = buffer.WriteByte(uint8(flagsDIO))
+	if err != nil {
+		return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error writing byte flags ADI: %w", err)
 	}
 
+	err = buffer.WriteByte(subr.DigitalOutputs)
+	if err != nil {
+		return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error writing DOUT: %w", err)
+	}
+
+	flagsBitsANS := ""
+	for i := len(subr.ANSExists) - 1; i >= 0; i-- {
+		if subr.ANSExists[i] != "0" && subr.ANSExists[i] != "1" {
+			return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Invalid ANS flag %d", i+1)
+		}
+		flagsBitsANS += subr.ANSExists[i]
+	}
 	flagsANS := uint64(0)
-	for i := len(subr.ANSExists)/2 - 1; i >= 0; i-- { // reversed order of ANS
-		opp := len(subr.ANSExists) - 1 - i
-		subr.ANSExists[i], subr.ANSExists[opp] = subr.ANSExists[opp], subr.ANSExists[i]
-	}
-	flagsANS, err = strconv.ParseUint(strings.Join(subr.ANSExists, ""), 2, 8)
+	flagsANS, err = strconv.ParseUint(flagsBitsANS, 2, 8)
 	if err != nil {
-		return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error writing flags ANS")
+		return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error writing flags ANS: %w", err)
 	}
-	if err = buffer.WriteByte(uint8(flagsANS)); err != nil {
-		return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error writing byte flags ANS")
+	err = buffer.WriteByte(uint8(flagsANS))
+	if err != nil {
+		return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error writing byte flags ANS: %w", err)
 	}
 
 	for i := range subr.DIOExists {
 		if subr.DIOExists[i] == "1" {
-			if err = buffer.WriteByte(subr.ADI[i]); err != nil {
-				return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error writing ADI")
+			err = buffer.WriteByte(subr.ADI[i])
+			if err != nil {
+				return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error writing ADI%d: %w", i+1, err)
 			}
 		}
 	}
 
 	for i := range subr.ANSExists {
 		if subr.ANSExists[i] == "1" {
+			if subr.ANS[i] > 0xffffff {
+				return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; ANS%d exceeds 24 bits", i+1)
+			}
 			ans := make([]byte, 4)
 			binary.LittleEndian.PutUint32(ans, subr.ANS[i])
-			if _, err = buffer.Write(ans[:3]); err != nil {
-				return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error writing ANS")
+			_, err = buffer.Write(ans[:3])
+			if err != nil {
+				return nil, fmt.Errorf("EGTS_SR_AD_SENSORS_DATA; Error writing ANS%d: %w", i+1, err)
 			}
 		}
 	}
